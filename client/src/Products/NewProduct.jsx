248dd520b90eca9f31b993c158e8bd9fe5a68ca3
Muneebtahir2002 <101181873+Muneebtahir2002@users.noreply.github.com>
2022-06-15 12:39:22 -0700

    A

--- client/src/Products/NewProduct.jsx
+++ client/src/Products/NewProduct.jsx
@@ -7,43 +7,41 @@ const NewProduct = (props) => {
   const [name, setname] = React.useState([""]);
   const [price, setprice] = React.useState([0]);
   return (
-    <Auth>
-      <Grid>
-        <Grid container spacing={3}>
-          <Grid item xs={3}>
-            <h2>Add new products</h2>
-            <TextField
-              label="Name"
-              variant="standard"
-              value={name}
-              onChange={(e) => {
-                setname(e.target.value);
-              }}
-            />
+    <Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <h2>Add new products</h2>
+          <TextField
+            label="Name"
+            variant="standard"
+            value={name}
+            onChange={(e) => {
+              setname(e.target.value);
+            }}
+          />
 
-            <TextField
-              label="Price"
-              variant="standard"
-              value={price}
-              onChange={(e) => {
-                setprice(e.target.value);
-              }}
-            />
-          </Grid>
+          <TextField
+            label="Price"
+            variant="standard"
+            value={price}
+            onChange={(e) => {
+              setprice(e.target.value);
+            }}
+          />
         </Grid>
-        <Button
-          variant="contained"
-          onClick={(e) => {
-            console.log("Send API Call");
-            productsservice.addproduct({ name, price }).then((data, _id) => {
-              props.history.push("/products");
-            });
-          }}
-        >
-          Add
-        </Button>
       </Grid>
-    </Auth>
+      <Button
+        variant="contained"
+        onClick={(e) => {
+          console.log("Send API Call");
+          productsservice.addproduct({ name, price }).then((data, _id) => {
+            props.history.push("/products");
+          });
+        }}
+      >
+        Add
+      </Button>
+    </Grid>
   );
 };
 
--- client/src/Products/UpdateProducts.jsx
+++ client/src/Products/UpdateProducts.jsx
@@ -15,48 +15,46 @@ const UpdateProducts = (props) => {
     });
   }, []);
   return (
-    <Admin>
-      <Grid>
-        <Grid container spacing={3}>
-          <Grid item xs={3}>
-            <h2>Update Product</h2>
-            <TextField
-              label="Name"
-              variant="standard"
-              value={name}
-              onChange={(e) => {
-                setname(e.target.value);
-              }}
-            />
-            {}
-            <TextField
-              label="Price"
-              variant="standard"
-              value={price}
-              onChange={(e) => {
-                setprice(e.target.value);
-              }}
-            />
-          </Grid>
+    <Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <h2>Update Product</h2>
+          <TextField
+            label="Name"
+            variant="standard"
+            value={name}
+            onChange={(e) => {
+              setname(e.target.value);
+            }}
+          />
+          {}
+          <TextField
+            label="Price"
+            variant="standard"
+            value={price}
+            onChange={(e) => {
+              setprice(e.target.value);
+            }}
+          />
         </Grid>
-        <Button
-          variant="contained"
-          onClick={(e) => {
-            productsservice
-              .updateProducts(id, { name, price })
-              .then((data) => {
-                console.log(data);
-                props.history.push("/products");
-              })
-              .catch((err) => {
-                console.log(err);
-              });
-          }}
-        >
-          Update
-        </Button>
       </Grid>
-    </Admin>
+      <Button
+        variant="contained"
+        onClick={(e) => {
+          productsservice
+            .updateProducts(id, { name, price })
+            .then((data) => {
+              console.log(data);
+              props.history.push("/products");
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }}
+      >
+        Update
+      </Button>
+    </Grid>
   );
 };
 
